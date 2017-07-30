9b77ccc90859f255e334a26fd89d7fe4a9f19ef0
Debakar <allinonedibakar@gmail.com>
2017-07-30 23:05:54 +0530

    Project Euler #6: Sum square difference

---
+++ src/Sum_square_difference.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Sum_square_difference {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int t;
+		long n, sumOfSquare, squareOfSum, sum;
+
+		t = input.nextInt();
+
+		while (t > 0) {
+			n = input.nextLong();
+			sumOfSquare = n * (n + 1) * (2 * n + 1) / 6;
+			sum = n * (n + 1) / 2;
+			squareOfSum = sum * sum;
+			System.out.println(squareOfSum - sumOfSquare);
+			t--;
+		}
+	}
+}
