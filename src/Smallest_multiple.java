1858552538a925957529aa00331609defbb24e2a
Debakar <allinonedibakar@gmail.com>
2017-07-30 21:51:16 +0530

    Project Euler #5: Smallest multiple

--- src/Smallest_multiple.java
+++ src/Smallest_multiple.java
@@ -2,7 +2,42 @@ import java.util.Scanner;
 
 public class Smallest_multiple {
 
+	public static boolean isPrime(int n) {
+		for (int i = 2; Math.pow(i, 2) <= n; i++) {
+			if (n % i == 0)
+				return false;
+		}
+		return true;
+	}
+
+	public static double maxPow(int i, int n) {
+		int j = 1;
+		while (true) {
+			if (Math.pow(i, j) > n)
+				break;
+			j++;
+		}
+
+		return (j - 1);
+	}
+
 	public static void main(String[] args) {
-		long
+		int t, n, result;
+
+		Scanner input = new Scanner(System.in);
+		t = input.nextInt();
+		while (t > 0) {
+			result = 1;
+			n = input.nextInt();
+			for (int i = 2; i <= n; i++) {
+				if (isPrime(i)) {
+					result = (int) (result * Math.pow(i, maxPow(i, n)));
+				}
+
+			}
+			System.out.println(result);
+			t--;
+		}
+
 	}
 }
