c757829a663e859d3b1d3061a37804c7638420d9
Dibakarroy1997 <allinonedibakar@gmail.com>
2017-07-30 21:53:28 +0530

    Project Euler #1: Multiples of 3 and 5

--- src/Multiples_of_3_and_5.java
+++ src/Multiples_of_3_and_5.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Multiples_of_3_and_5 {
+public class Multiples_of_3_and_5 { 
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
